5059af9565b402612be24f9a6226d8e0da396eac
Rohitprasad83 <pdrohit674@gmail.com>
2022-04-21 23:54:14 +0530

    mouseenter and mouseleave event for bold texts

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 import './style.css';
 
 // Write Javascript code!
+const getAllBoldText = document.querySelectorAll('strong');
+
+getAllBoldText.forEach((item) => {
+  item.addEventListener('mouseenter', () => {
+    item.style.color = 'green';
+  });
+  item.addEventListener('mouseleave', () => {
+    item.style.color = 'black';
+  });
+});
